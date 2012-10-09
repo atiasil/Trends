ad9062d125c90fdd55b8db5fde3d23342b2099bc
atiasil <ilan.atias@gmail.com>
2012-10-09 17:15:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trends::Application.config.secret_token = 'cd3d7c55737e82b8fcd7a948f3d34e14df91ae0cf88b929070fe81e999fbcc5a6db8bb17655e56e7126e971381564c1b41cfe4077b34f43fb79e665329e780e3'
